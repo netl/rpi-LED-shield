08482fb02788d25f0266bf1950495c151ada42cf
juuso Metsavuori <juuso.metsavuori@gmail.com>
2016-02-16 14:35:13 +0000

    now seperate inputs are tweakable

--- src/configure.py
+++ src/configure.py
@@ -5,16 +5,16 @@ bus = smbus.SMBus(1)
 
 #setup
 
-current = 0.35 #led current
-shuntResistor = 0.1 #value of current shunt on board
-calculatedCurrent = int(current*shuntResistor*0xff/2)
+current = [0.20,0.35,0.1] #led currents R,G,B
+shuntResistor = 0.2 #value of current shunt on board
 
 #set reference to 2.0V
 bus.write_i2c_block_data(0x1f,0x76,[0,0])
 
 #set currents
-print("setting outputs to ", current,"A (",calculatedCurrent,")")
 
 #set all outputs to calculatedCurrent
-bus.write_i2c_block_data(0x1f,0x80,[calculatedCurrent,0])
-bus.write_i2c_block_data(0x1f,0x81,[0,0])
+for led in range(len(current)):
+   calculatedCurrent = int(current[led]*shuntResistor*0xff/2)
+   print("setting output", led, " to ", current[led],"A (",calculatedCurrent,")")
+   bus.write_i2c_block_data(0x1f,0x20+led,[calculatedCurrent,0])
