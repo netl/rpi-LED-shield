389b89c4f785d0c24e28320377efd563c2bf1157
Juuso Metsävuori <juuso.metsavuori@gmail.com>
2016-07-27 20:27:05 +0000

    changed current calculations

--- src/configure.py
+++ src/configure.py
@@ -11,7 +11,7 @@ bus = smbus.SMBus(1)
 
 #setup
 current=[float(sys.argv[x]) for x in range(1,5)]
-shuntResistor = 0.43 #value of current shunt on board
+shuntResistor = 0.2 #value of current shunt on board
 estimatedVoltage = 12 #voltage on supply
 
 #set reference to 2.0V
@@ -20,7 +20,8 @@ bus.write_i2c_block_data(0x1f,0x76,[0,0])
 #set all outputs to desired current
 totalPower = 0
 for output in range(len(current)):
-   calculatedCurrent = int(current[output]*shuntResistor*0xff/2)
+   #calculatedCurrent = int(current[output]*shuntResistor*0xff/2)
+   calculatedCurrent = int(current[output]*0xff/1.56)
    totalPower += current[output]
    print("setting output", output, " to ", current[output],"A (",calculatedCurrent,")")
    bus.write_i2c_block_data(0x1f,0x20+output,[calculatedCurrent,0])
